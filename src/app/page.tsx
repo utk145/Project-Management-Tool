0fead9da370c410bacd8558350edd19ab225a849
utk145 <utkarshs14.2002@gmail.com>
2025-02-02 13:10:56 +0530

    customizing components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div>
-      Hello world
-      <Button variant={"ghost"}>button</Button>
-      <p className="text-red-500 font-bold">Luffy</p>
+    <div className="">
+      <Button variant={"primary"}>Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="territory">Territory</Button>
+      <Input type="text" placeholder="Type here" />
     </div>
   );
 }
