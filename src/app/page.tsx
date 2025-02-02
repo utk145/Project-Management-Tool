debd5a063284a07c5d1d0fdebb70224e93211b1a
utk145 <utkarshs14.2002@gmail.com>
2025-02-02 14:13:12 +0530

    config: eslint and prettier configurations and addition of scripts to check and resolve them

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,16 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
 export default function Home() {
-  return (
-    <div className="">
-      <Button variant={"primary"}>Primary</Button>
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="destructive">Destructive</Button>
-      <Button variant="ghost">Ghost</Button>
-      <Button variant="outline">Outline</Button>
-      <Button variant="muted">Muted</Button>
-      <Button variant="territory">Territory</Button>
-      <Input type="text" placeholder="Type here" />
-    </div>
-  );
+    return (
+        <div className="">
+            <Button variant={"primary"}>Primary</Button>
+            <Button variant="secondary">Secondary</Button>
+            <Button variant="destructive">Destructive</Button>
+            <Button variant="ghost">Ghost</Button>
+            <Button variant="outline">Outline</Button>
+            <Button variant="muted">Muted</Button>
+            <Button variant="territory">Territory</Button>
+            <Input type="text" placeholder="Type here" />
+        </div>
+    );
 }
