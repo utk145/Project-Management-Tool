fb4ebcdadd0a6771b073e313f963c6c37d645370
utk145 <utkarshs14.2002@gmail.com>
2025-02-22 19:15:18 +0530

    refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 
 import "./globals.css";
-import { QueryProvider } from "@/components/custom/query-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
