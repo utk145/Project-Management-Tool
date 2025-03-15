423ba8f2eeffe689fbb78551b0dd89557515968f
utk145 <utkarshs14.2002@gmail.com>
2025-03-15 10:03:26 +0530

    esllint disable for namespace error Error: ES2015 module syntax is preferred over namespaces.  @typescript-eslint/no-namespace

--- src/lib/commons/AllCommonsConstants.ts
+++ src/lib/commons/AllCommonsConstants.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+// - keeping the namespace for backward compatibility
 export namespace AllCommonsConstants {
     export enum StatusCodes {
         /**
