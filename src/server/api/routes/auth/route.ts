f383f8a1e4f8d3527ef7765450cd1f4a3d30c8b9
Sudhanshu kumar <sudhanshukr8971@gmail.com>
2025-03-16 16:19:04 +0530

    Removed the line

--- src/server/api/routes/auth/route.ts
+++ src/server/api/routes/auth/route.ts
@@ -5,12 +5,10 @@ import { signInSchema, signUpSchema } from "../../models/auth.schema";
 const authAppRouter = new Hono()
     .post("/login", zValidator("json", signInSchema), (c) => {
         const { email, password } = c.req.valid("json");
-        console.log({ email, password }); // IMPORTANT: Remove this line
         return c.json({ email, password });
     })
     .post("/register", zValidator("json", signUpSchema), (c) => {
         const { email, password, name } = c.req.valid("json");
-        console.log({ email, password, name }); // IMPORTANT: Remove this line
         return c.json({ email, password, name });
     });
 export default authAppRouter;
